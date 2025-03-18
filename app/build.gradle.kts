b190e2af800510676964248a9cc862b8bc3fdf7d
johnshapiro <johnhshapiro@gmail.com>
2025-03-18 13:32:29 -0600

    added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Retrofit
+    implementation(libs.retrofit)
+    // Retrofit with Scalar Converter
+    implementation(libs.converter.scalars)
 }
